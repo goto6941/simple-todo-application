5665c841b4b2232f40a95236477cd9cf90b3fcfc
kai1026-sh <kai.shimazaki@gmail.com>
2025-06-20 17:00:54 +0900

    コミット１

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function onUpdateIsDone(task) {
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     const currentValue = task.isDone;
-    task.isDone = false;
+    task.isDone = true;
 
     return task;
 }
